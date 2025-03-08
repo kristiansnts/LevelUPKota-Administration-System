fe32705775617a3464db69991af8b7c51866c5ca
kristiansnts-yourpay <kristian.santoso@yourpay.co.id>
2025-03-08 18:49:50 +0700

    fix(main): add shieldseeder for production

--- app/Livewire/CustomProfileComponent.php
+++ app/Livewire/CustomProfileComponent.php
@@ -40,10 +40,6 @@ class CustomProfileComponent extends Component implements HasForms
 
     public function form(Form $form): Form
     {
-
-        $user = auth()->user();
-        dump($user->roles->pluck('name'));
-
         return $form
             ->schema([
                 PersonalInfoForm::make(),
