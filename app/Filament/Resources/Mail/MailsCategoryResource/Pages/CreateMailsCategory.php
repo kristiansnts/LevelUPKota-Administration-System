bfaf60541985f9050114534f69e17e7c9c9dfb03
kristiansnts-yourpay <kristian.santoso@yourpay.co.id>
2025-03-10 18:35:50 +0700

    fix(resource): fix some doc error

--- app/Filament/Resources/Mail/MailsCategoryResource/Pages/CreateMailsCategory.php
+++ app/Filament/Resources/Mail/MailsCategoryResource/Pages/CreateMailsCategory.php
@@ -11,21 +11,16 @@ class CreateMailsCategory extends CreateRecord
 {
     protected static string $resource = MailsCategoryResource::class;
 
-    public function getRecord(): ?\App\Models\MailCategory
-    {
-        return $this->record;
-    }
-
     protected function afterCreate(): void
     {
-        if (! $this->record instanceof \Illuminate\Database\Eloquent\Model) {
+        if (! $this->record instanceof \App\Models\MailCategory) {
             return;
         }
 
         $user = ModelQueryService::getUserModel();
 
         MailCategoryUser::create([
-            'mail_category_id' => $this->getRecord()?->id,
+            'mail_category_id' => $this->record->id,
             'city_id' => $user->city_id,
             'district_id' => $user->district_id ?? null,
             'order' => 0,
